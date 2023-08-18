f4dbf6c57f55e8ffb3aca5dbf4161103e09f9e6b
Paul LeTourneau <pletourneau62@gmail.com>
2023-08-18 08:36:05 -0700

    add js to pass test 1

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Business Logic
+
+function beepBoop(num) {
+  const arr = [];
+  for (let i=0; i<=num; i++) {
+    arr.push(i);
+    console.log(arr);
+  }
+}
+
+
+
+// UI Logic
+
+//getNum function
