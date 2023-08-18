17891c09dedabde8d0a8b549edf3a8ac3a197a9d
Paul LeTourneau <pletourneau62@gmail.com>
2023-08-18 14:08:11 -0700

    add more UI logic and format spaces in return array

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,20 @@
 // Business Logic
 
-const num = document.querySelector("input")
 
 function beepBoop(num) {
   const arr = [];
   const inputNum = parseInt(num);
-  console.log(typeof(inputNum));
   if (inputNum > 0) {
     for (let i=0; i<=inputNum; i++) {
       let iStr = i.toString();
       if (iStr.includes("3")) {
-        arr.push("Won't you be my neighbor?");
+        arr.push(" Won't you be my neighbor?");
       } else if (iStr.includes("2")) {
-        arr.push("Boop");
+        arr.push(" Boop");
       } else if (iStr.includes("1")) {
-        arr.push("Beep");
+        arr.push(" Beep");
       } else {
-        (arr.push(i));
+        (arr.push(" " + i));
       }
     }
   } else if (inputNum < 0){
@@ -33,13 +31,11 @@ function beepBoop(num) {
       }
     }
   }
+        result = arr.toString(" ");
+  }
   
-  const result = arr.toString(" ");
-  console.log(result);
-  document.querySelector(".hidden").removeAttribute("class");
-  document.getElementById("output").innerText = result;
-  // document.getElementById("form").reset();
-}
+  
+
 
 
 
@@ -47,11 +43,15 @@ function beepBoop(num) {
 //UI Logic
 window.addEventListener("load", function() {
   const form = document.getElementById("form");
+  document.getElementById("output").setAttribute("class", "hidden"); 
   form.addEventListener("submit", handleSubmission); 
 });
 
 function handleSubmission(event) {
   event.preventDefault();
-  document.getElementById("output").setAttribute("class", "hidden");
+  const num1 = document.getElementById("q1").value;
+  beepBoop(num1); 
+  document.querySelector(".hidden").removeAttribute("class");
+  document.getElementById("output").innerText = result;
+  document.getElementById("form").reset();
 }
-
